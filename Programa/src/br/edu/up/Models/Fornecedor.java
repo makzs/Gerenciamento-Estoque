08853bc2f46ca7ae505f18869d6031fe87c6b301
Hiro <162133006+hirowatanabe01@users.noreply.github.com>
2024-06-07 10:27:40 -0300

    Update Fornecedor.java

--- Programa/src/br/edu/up/Models/Fornecedor.java
+++ Programa/src/br/edu/up/Models/Fornecedor.java
@@ -1,5 +1,38 @@
 package br.edu.up.Models;
 
 public class Fornecedor extends BaseEntity {
+   private String nome;
+   private int cnpj;
+   private String end;
+
+   public Fornecedor (String nome, int cnpj, String end) {
+      this.nome = nome;
+      this.cnpj = cnpj;
+      this.end = end;
+   }
+
+   public String getNome() {
+      return nome;
+   }
+
+   public int getCnpj() {
+      return cnpj;
+   }
+
+   public String getEnd() {
+      return end;
+   }
+
+   public void setNome(String nome) {
+      this.nome = nome;
+   }
+
+   public void setCnpj(int cnpj) {
+      this.cnpj = cnpj;
+   }
+
+   public void setEnd(String end) {
+      this.end = end;
+   }
 
 }
