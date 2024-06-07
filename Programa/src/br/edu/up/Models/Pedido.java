125c56305b1116d567a0c65d0df8822d0c00ec39
Hiro <162133006+hirowatanabe01@users.noreply.github.com>
2024-06-07 10:43:36 -0300

    first commit Pedido
    don't forget to change formating!!!

--- Programa/src/br/edu/up/Models/Pedido.java
+++ Programa/src/br/edu/up/Models/Pedido.java
@@ -1,5 +1,37 @@
 package br.edu.up.Models;
 
 public class Pedido extends BaseEntity {
+   private int id;
+   private String produto;
+   private String cliente;
 
+   public Pedido(int id, String produto, String cliente) {
+      this.id = id;
+      this.produto = produto;
+      this.cliente = cliente;
+   }
+
+   public int getId() {
+      return id;
+   }
+
+   public String getProduto() {
+      return produto;
+   }
+
+   public String getCliente() {
+      return cliente;
+   }
+
+   public void setId(int id) {
+      this.id = id;
+   }
+
+   public void setProduto(String produto) {
+      this.produto = produto;
+   }
+
+   public void setCliente(String cliente) {
+      this.cliente = cliente;
+   }
 }
