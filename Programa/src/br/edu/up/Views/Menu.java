15ae05946a28a6a55e1c8def1ed882b7d038a065
makzs <erikmakiak@gmail.com>
2024-06-07 10:10:41 -0300

    menu

--- Programa/src/br/edu/up/Views/Menu.java
+++ Programa/src/br/edu/up/Views/Menu.java
@@ -1,5 +1,56 @@
 package br.edu.up.Views;
 
+import java.util.Scanner;
+
 public class Menu {
+    public void Mostrar() {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("---------------------------");
+        System.out.println("    MENU PRINCIPAL  ");
+        System.out.println("---------------------------");
+
+        System.out.println("1. Endereço");
+        System.out.println("2. Cliente");
+        System.out.println("3. Fornecedor");
+        System.out.println("4. Produto");
+        System.out.println("5. Pedido");
+        System.out.println("---------------------------");
+        System.out.println("Opção: ");
+        int opcao = leitor.nextInt();
+
+        switch (opcao) {
+            case 1:
+                // menu endereço;
+                break;
+            case 2:
+                MenuCliente();
+                break;
+            case 3:
+                // menu fornecodr
+                break;
+            case 4:
+                // menu produto;
+                break;
+            case 5:
+                // menu pedido
+                break;
+            default:
+                System.out.println("Opção Invalida!");
+                break;
+        }
+
+        leitor.close();
+    }
+
+
+    public void MenuCliente(){
+        
+        System.out.println("---------------------------");
+        System.out.println("    MENU CLIENTE  ");
+        System.out.println("---------------------------");
+
+
+    }
 
 }
