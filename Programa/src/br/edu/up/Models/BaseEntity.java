554e625d34785917d93dcf64c52e2b92b6d42abb
viniciustravenssoli <vinicioussouza@gmail.com>
2024-06-07 09:31:22 -0300

    adding models

---
+++ Programa/src/br/edu/up/Models/BaseEntity.java
@@ -0,0 +1,6 @@
+package br.edu.up.Models;
+
+public abstract class BaseEntity {
+
+    public int Id;
+}
--- Programa/src/br/edu/up/Models/Item.java
+++
@@ -1,8 +0,0 @@
-package br.edu.up.Models;
-
-public abstract class Item {
-    private String id;
-    private String nome;
-    private Double preco;
-    private int qtdEstoque;
-}
